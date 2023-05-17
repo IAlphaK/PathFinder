ab15b52ecbcf1e326980fba34e3d6e99972aee19
Abdullah Basit <alpha.ab@yahoo.com>
2023-05-18 03:47:35 +0500

    add node functionality

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from flask import app
-
 import python_ui
 from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox, QGraphicsItem
 from PyQt6.QtCore import Qt
@@ -11,13 +9,36 @@ app = QApplication(sys.argv)
 main_window = QMainWindow()
 interface = python_ui.Ui_MainWindow()
 interface.setupUi(main_window)
-##########################################################################################
+
+# Store references to the qlines
+node1_qline = interface.i_n1
+node2_qline = interface.i_n2
+weight_qline = interface.i_weight
+
+def add_node():
+    # Get the text entered in qlines
+    node1 = node1_qline.text()
+    node2 = node2_qline.text()
+    weight = weight_qline.text()
+
+    # Perform validation on the inputs
+    if len(node1) != 1 or len(node2) != 1:
+        QMessageBox.warning(main_window, "Invalid Input", "Node names should be a single letter.")
+        return
+
+    if not weight.isdigit():
+        QMessageBox.warning(main_window, "Invalid Input", "Weight should be an integer.")
+        return
+
+    # Clear the qlines
+    node1_qline.clear()
+    node2_qline.clear()
+    weight_qline.clear()
 
 
 
 
 
-##########################################################################################
+interface.node_add.clicked.connect(add_node)
 main_window.show()
-
 sys.exit(app.exec())
