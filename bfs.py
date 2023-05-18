ff6804b882c08370c943fa2110f043c7d4b6aec9
Abdullah Basit <alpha.ab@yahoo.com>
2023-05-18 19:28:25 +0500

    implemented BFS

--- bfs.py
+++ bfs.py
@@ -6,41 +6,25 @@ class Graph:
         self.graph = defaultdict(list)
 
     # function to add an edge to graph
-    def addEdge(self, u, v):
-        self.graph[u].append(v)
+    def addEdge(self, u, v, weight, directed=True):
+        self.graph[u].append((v, weight, directed))
+        if not directed:
+            self.graph[v].append((u, weight, directed))
 
     # Function to print a BFS of graph
-    def BFS(self, s,find):
-        queu = []
+    def printpath(self, start, goals):
+        queue = []
         visited = []
-        queu.append(s)
-        visited.append(s)
-        while queu:
-            s = queu.pop(0)
-            print(s)
-            if s==find:
-                print("Number has been found")
-                exit()
-            for i in self.graph[s]:
+        queue.append((start, []))  # Store the path along with each vertex
+        visited.append(start)
+        while queue:
+            vertex, path = queue.pop(0)
+            path.append(vertex)
+            print(vertex)
+            if vertex in goals:
+                return path
+            for i, weight, directed in self.graph[vertex]:
                 if i not in visited:
-                    queu.append(i)
+                    if not directed or (directed and i not in path):
+                        queue.append((i, list(path)))  # Create a new copy of the path for each neighbor
                     visited.append(i)
-
-# Driver code
-
-# Create a graph given in
-# the above diagram
-g = Graph()
-g.addEdge(1, 2)
-g.addEdge(1, 3)
-g.addEdge(2, 4)
-g.addEdge(2, 5)
-g.addEdge(3, 6)
-g.addEdge(3, 7)
-
-g.addEdge(2, 1)
-g.addEdge(3, 1)
-g.addEdge(4, 2)
-g.addEdge(5, 2)
-g.addEdge(6, 3)
-g.addEdge(7, 3)
